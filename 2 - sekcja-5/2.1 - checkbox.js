9aab4cf9fe19bbc3a3159d6dd41fae2e66b92572
Marek Trybuła <trybu001@gmail.com>
2022-07-25 09:31:25 +0200

    destrukturyzacja cz2

--- 2 - sekcja-5/2.1 - checkbox.js
+++ 2 - sekcja-5/2.1 - checkbox.js
@@ -1,21 +1,18 @@
-// const PositiveMessage = () => <p>Możesz kupić karton trojanów gratulacje!</p>;
-// const NegativeMessage = () => <p>Musisz jeszcze poczekać...</p>;
-
-const displayMessage = (isConfirmed, isFormSubmited) => {
-  if (isFormSubmited) {
-    if (isConfirmed === true) {
-      return <ValidationMessage txt="Możesz kupić karton trojanów" />;
-    } else {
-      return <ValidationMessage txt="Musisz dorosnąć!" />;
-    }
-  } else return null;
-};
-
 const ValidationMessage = (props) => {
   const { txt } = props; //Można podać kilka atrybutów np const {txt, title, count} = props;
   return <p>{txt}</p>;
 };
-
+const OrderForm = (props) => {
+  const { submit, isConfirmed, change } = props;
+  return (
+    <form onSubmit={submit}>
+      <input type="checkbox" id="age" checked={isConfirmed} onChange={change} />
+      <label htmlFor="age">Mam co najmniej 18 lat</label>
+      <hr />
+      <button type="submit">Kup Bilet</button>
+    </form>
+  );
+};
 class TicketShop extends React.Component {
   state = {
     isConfirmed: false,
@@ -38,15 +35,15 @@ class TicketShop extends React.Component {
     }
   };
 
-  // displayMessage = () => {
-  //   if (this.state.isFormSubmited) {
-  //     if (this.state.isConfirmed === true) {
-  //       return <ValidationMessage txt="Możesz kupić karton trojanów" />;
-  //     } else {
-  //       return <ValidationMessage txt="Musisz dorosnąć!" />;
-  //     }
-  //   } else return null;
-  // };
+  displayMessage = () => {
+    if (this.state.isFormSubmited) {
+      if (this.state.isConfirmed === true) {
+        return <ValidationMessage txt="Możesz kupić karton trojanów" />;
+      } else {
+        return <ValidationMessage txt="Musisz dorosnąć!" />;
+      }
+    } else return null;
+  };
 
   render() {
     const { isConfirmed, isFormSubmited } = this.state; //destrukturyzacja - wyciąganie wartości z tablicy, obiektu i przypisywanie do zmiennej, dzięki temu poniżej zamiast pisać this.state.isConfirmed możemy napisać po prostu isConfirmed
@@ -54,18 +51,12 @@ class TicketShop extends React.Component {
     return (
       <>
         <h1>Kup karton trojanów za połowę ceny!</h1>
-        <form onSubmit={this.handleFormSubmit}>
-          <input
-            type="checkbox"
-            id="age"
-            checked={isConfirmed}
-            onChange={this.handleCheckboxChange}
-          />
-          <label htmlFor="age">Mam co najmniej 18 lat</label>
-          <hr />
-          <button type="submit">Kup Bilet</button>
-        </form>
-        {displayMessage(isConfirmed, isFormSubmited)}
+        <OrderForm
+          change={this.handleCheckboxChange}
+          submit={this.handleFormSubmit}
+          checked={isConfirmed}
+        />
+        {this.displayMessage()}
       </>
     );
   }
